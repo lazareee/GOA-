43d00d6f88dd0333c72c703aeeefc5775a8cb91b
lazareee <purichamiashvililazare@gmail.com>
2025-10-17 23:21:14 +0400

    second commit

---
+++ day-2/homework-2.py
@@ -0,0 +1,83 @@
+# 2) debugging გვეხმარება ვნახოთ არის თუარა კოდში შეცდომები. იმიტომ იქნება ერორი რომ პრინტ ბრძანება გაცემული არის ცვლადის შექმნამდე.
+# 3)
+#natural_number = 1
+
+#decimal_number = 0.1
+
+#print(natural_number + decimal_number)
+
+# 4)
+
+#number_one = 4
+
+#number_two = 2
+
+#print(number_one - number_two)
+
+#print(number_one + number_two)
+
+#print(number_one * number_two)
+
+#print(number_one / number_two)
+
+
+# 5)
+
+#age = 25
+
+#print(age + 5)
+
+
+# 6)
+
+
+#name = "ლაზარე"
+#surname = "პურიჩამიაშვილი"
+#age = 16
+#height = 1.78
+#city = "თბილისი"
+
+# f-string
+#sentence_f = f"მე ვარ {name} {surname}, ვარ {age} წლის, ჩემი სიმაღლე {height} მ და ვცხოვრობ {city}-ში."
+#print(sentence_f)
+
+
+# 7) ერორს
+
+
+
+# 8)
+
+#total = 120
+
+#people = 4
+
+#one_person = 120/4
+
+#print(one_person)
+
+
+# 9)
+
+#price = 80
+
+#disscount = 0.5
+
+#final_price = price - (price * disscount)
+
+#print(final_price)
+
+
+
+# 10)
+
+number = 7
+
+if number % 2 == 0:
+    print("The number is even")  
+else:
+    print("The number is odd")   
+
+
+
+
