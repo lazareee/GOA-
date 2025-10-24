fe43966cbea0f978138e8ce31452dc2d80ba7681
lazareee <purichamiashvililazare@gmail.com>
2025-10-24 22:01:52 +0400

    second commit

---
+++ day-3/homework-3.py
@@ -0,0 +1,67 @@
+# 1)
+
+#a = 5
+#b = 3
+
+#sum = 5 + b
+#print(sum)
+
+
+# 2)
+
+#x = "10"       
+#y = int(x)     
+
+#print(y + 5)
+
+
+# 3)
+
+#x = float(input("შეიყვანე პირველი რიცხვი: "))
+#y = float(input("შეიყვანე მეორე რიცხვი: "))
+
+#print(x + y)
+
+# 4)
+
+#num1 = int(input("Enter the first integer number: "))
+#num2 = int(input("Enter the second integer number: "))
+
+
+#if num1 % 2 == 0 and num2 % 2 == 0:
+    #print("Both numbers are even, their sum is:", num1 + num2)
+#else:
+    #print("The given numbers are not even so they will not be summed")
+
+
+
+# 5)
+
+#name = input("Enter your name: ")
+#surname = input("Enter your surname: ")
+#age = input("Enter your age: ")
+#city = input("Enter your city: ")
+#country = input("Enter your country: ")
+
+
+#print("Your name is:", name)
+#print("Your surname is:", surname)
+#print("Your age is:", age)
+#print("You live in:", city)
+#print("The country you live in is:", country)
+
+
+# 6)
+
+#x = 5
+#y = 5
+
+#print(x == y)
+
+
+a = 3
+b = 7
+
+print(a != b)
+
+
