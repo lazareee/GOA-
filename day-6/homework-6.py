0bd9223dac7ad027c37876848c71a898109f7d74
lazareee <purichamiashvililazare@gmail.com>
2025-11-15 19:12:57 +0400

    second commit

---
+++ day-6/homework-6.py
@@ -0,0 +1,95 @@
+#1
+
+#password = "lazare123"
+#attempts = 0
+
+#while attempts < 3:
+   #user_input = input("Enter password: ")
+    #if user_input == password:
+        #print("Correct password!")
+        #break
+    #else:
+        #attempts += 1
+        #print("Incorrect password!")
+
+#if attempts == 3:
+    #print("You have reached the maximum number of attempts")
+
+
+
+
+#2
+
+# TRUE
+
+
+#3
+
+
+# მასივი (ან სიები Python-ში) არის მონაცემთა სტრუქტურა,
+# სადაც შეგვიძლია შევინახოთ ბევრი მნიშვნელობა ერთ ცვლადში.
+
+
+# დიახ Python-ში სიაში თავისუფლად შეგვიძლია ჩავდოთ
+# str, int, float, boolean და სხვა მონაცემთა ტიპებიც.
+
+
+#4
+
+
+#family = ["lazare", "dachi", "elene", "giorgi", "ciko", "oliko"]
+
+#for member in family:
+    #print(member)
+
+
+#5
+
+#numbers = [5, 10, 20, 15, 50]
+
+#total = 0
+
+#for num in numbers:
+    #total += num
+
+#print("Sum:", total)
+
+
+#6
+
+
+# Indexing არის პროცესი, როცა ჩვენ ვუკავშირდებით სტრიქონის, სიის ან სხვა მონაცემთა სტრუქტურის
+# კონკრეტულ ელემენტს მისი ინდექსის მიხედვით.
+
+
+# მაგალითად:  "Hello"
+# H ->  0
+# e ->  1
+# l ->  2
+# l ->  3
+# o ->  4
+
+
+#7
+
+# Indexing გვაძლევს საშუალებას, სწრაფად და მარტივად მივწვდეთ
+# სიაში, სტრიქონში ან სხვა სტრუქტურაში არსებულ კონკრეტულ ელემენტს.
+# ეს ბევრად მარტივია და ეფექტური, ვიდრე ყველა ელემენტზე გავლა.
+
+
+
+#8
+
+#s = "Giorgi"
+#print(s[0])
+
+
+#s = "Mariam"
+#print(s[-2])
+
+
+name = "Nina"
+first = name[0]
+last = name[-1]
+new = first + last
+print(new)
